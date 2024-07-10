a4747a4512508d7c5152d7e86929a472f44c90ac
Deivison Gonçalves <deivisoncpmg201@gmail.com>
2024-07-10 00:13:36 -0300

    Integração com API
    Integração com a API e processo de conversão de moedas

--- script.js
+++ script.js
@@ -14,14 +14,102 @@ function ativaDarkMode(){
 const btnDarkMode = document.getElementById('btnDarkMode')
 btnDarkMode.addEventListener('click', ativaDarkMode)
 
+/* -------------------------- */
 
-/* Função Consulta API bandeiras */
-function consultaApiBandeira(){
-    const api = fetch("https://restcountries.com/v2/all")
-    .then((res) => {
-        res.json().then((dados) => {
-            console.log(dados[0].flags.svg)
-        })
-    })
+
+
+/* Criando Select Moedas apartir da API */ 
+
+async function selectMoedas(){
+    /* Pegando dados da API */ 
+    let moedas = []
+    
+    try{
+        const res = await fetch("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json")
+
+        const dados = await res.json()
+        
+        moedas = dados
+        
+    } catch(error){
+        console.error("Erro ao buscar Moedas: ", error)
+    }
+
+    /* Função que cria o select que pode ser usado nas 2 moedas */
+    function criaSelect(id){
+        const select = document.getElementById(id)
+
+        Object.keys(moedas).forEach(x => {
+
+            if(moedas[x] != ''){
+                const option = document.createElement('option')
+                option.value = x
+                option.textContent = `${x} - ${moedas[x]}`
+    
+                select.append(option)
+            }    
+        });
+
+    }
+    criaSelect('listaMoeda1')
+    criaSelect('listaMoeda2')
+
 }
-consultaApiBandeira()
+selectMoedas()
+/* ----------------------------------------- */
+
+
+
+
+/* Função para converter os valores */
+
+const btnConverter = document.getElementById('btnConverter')
+btnConverter.addEventListener('click', () => converte('moeda1', 'moeda2'))
+
+
+function converte(x, y){
+    const moeda1 = document.getElementById(x).value
+    const moeda2 = document.getElementById(y).value
+
+    const inputMoeda1 = parseFloat(document.getElementById('inputMoeda1').value)
+
+    /* Buscando dados da API */
+    let retornoBusca = []
+
+    async function buscaDados(){
+        try{
+            const api = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024.7.9/v1/currencies/${moeda1}.json`)
+
+            const dados = await api.json()
+
+            retornoBusca = dados
+        }catch(error){
+            console.error('Erro ao buscar dados: ', error)
+        }
+
+        const msgErro = document.getElementById('msgErro')
+        msgErro.textContent = ''
+        msgErro.style.padding = '0px'
+
+        if(!isNaN(inputMoeda1)){
+            
+            let resposta = inputMoeda1*retornoBusca[moeda1][moeda2]
+
+            document.getElementById('inputMoeda2').value = resposta
+            
+        }else{
+            msgErro.textContent = 'Ops, Digite um valor válido!!!'
+            msgErro.style.padding = '8px 15px'
+        }
+               
+    
+    }
+    buscaDados()
+
+    
+
+}
+
+
+
+
